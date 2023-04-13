2abc0da69366500ebe22b483054f1e91d571bb03
Juan Gomez <juandagomezl12@gmail.com>
2023-04-13 03:28:11 -0500

    add dag 2

---
+++ dags/ops/dag_2.py
@@ -0,0 +1,12 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from datetime import datetime
+
+with DAG(dag_id="jeffer_se_va_2",
+         description="Se va para ecuador",
+         start_date= datetime(2023, 4, 17),
+         schedule_interval="0 12 * * *") as dag:
+    
+    t1 = EmptyOperator(task_id="task_1")
+
+    t1

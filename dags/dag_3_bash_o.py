9b762aa82277e2e0ebecf23c6fe4ec57b96a0d9c
Juan Gomez <juandagomezl12@gmail.com>
2023-04-13 03:58:54 -0500

    ss

--- dags/dag_3_bash_o.py
+++ dags/dag_3_bash_o.py
@@ -5,9 +5,9 @@ from datetime import datetime
 
 with DAG(dag_id="Bash_Operator",
          description="Segundo Operador",
-         start_date= datetime(2023, 4, 17)) as dag:
-    
-    t1 = BashOperator(task_id="task_3", 
-                    bash_command="echo 'Los quiero'")
+         start_date=datetime(2023, 4, 17)) as dag:
+
+    t1 = BashOperator(task_id="task_3",
+                      bash_command="echo 'Los quiero'")()
 
     t1

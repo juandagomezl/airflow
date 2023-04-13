69fcd07c894ab5c8fca975fcec2797f5f6b26be3
Juan Gomez <juandagomezl12@gmail.com>
2023-04-13 03:52:07 -0500

    ss

---
+++ dags/dag_4_python_o.py
@@ -0,0 +1,16 @@
+from airflow import DAG
+from airflow.operator.python import PythonOperator
+from datetime import datetime
+
+def print_data():
+    print("hola")
+
+with DAG(dag_id="Python_Operator",
+         description="primer dag con python",
+         schedule_interval="@once",
+         start_date=datetime(2024, 4, 18)) as dag:
+    
+    t1 = PythonOperator(task_id="task_6",
+                        python_callable=print_data)
+    
+    t1
